cf1c3b05ee5ba460cf5d403d1081883601c2dc46
王思渊 <31314554@qq.com>
2022-01-24 16:25:26 +0800

    :fire:

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+      <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const msg = ref('welcome to use');
+    return {
+      msg,
+    };
+  },
+});
+</script>
+<style lang="less" scoped>
+
+</style>
